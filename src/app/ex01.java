e0f78451f8bb9ea59323d84cd2fd8fcb4f08c4ed
karol <kiellenkarolinne123@icloud.com>
2024-01-05 19:16:05 -0300

    variável do tipo double

---
+++ src/app/ex01.java
@@ -0,0 +1,16 @@
+package app;
+
+public class ex01 {
+
+        public static void main(String[] args){
+
+            int x, y;
+            x = 5;
+            y = 2 * x;
+
+            System.out.println(x);
+            System.out.println(y);
+
+        }
+    }
+
